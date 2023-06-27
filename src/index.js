d602e3a87a88a98f358359b9e82280dd996b92fd
Aaron <arnhsmith@gmail.com>
2023-06-27 12:13:50 -0400

    Adds different json port

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
+// server port:
+
+// http://127.0.0.1:5500/
+
 let currentMeme;
 let navBar = document.getElementById("meme-menu");
-
-const codingMemesAPI = "https://meme-api.com/gimme/ProgrammerHumor/10/";
-const memesAPI = "https://meme-api.com/gimme/ProgrammerHumor/10";
+let memeData;
+// const codingMemesAPI = "https://meme-api.com/gimme/ProgrammerHumor/10/";
+const memesAPI = "http://localhost:5500/memes";
 
 function loadMemes() {
   fetch(memesAPI)
-    .then((imgFlipResp) => imgFlipResp.json())
+    .then((resp) => resp.json())
 
     .then((APIData) => {
-      let memeData = APIData.memes;
+      memeData = APIData;
+      // console.log(memeData);
       memeData.forEach((memeData) => navMenu(memeData));
 
-      memeDetails(APIData.memes[0]);
-      console.log(APIData.memes[0]);
+      memeDetails(APIData[0]);
+      // console.log(APIData[0].image);
     });
 }
 loadMemes();
@@ -21,21 +26,76 @@ loadMemes();
 function navMenu(memeData) {
   const menuImg = document.createElement("img");
 
-  console.log(memeData);
-  menuImg.src = memeData.url;
-  console.log(menuImg);
+  // console.log(memeData);
+  menuImg.src = memeData.image;
 
   navBar.append(menuImg);
 
   menuImg.addEventListener("click", () => {
-    memeDetails(codingMemesAPI);
+    memeDetails(memeData);
   });
 }
 
-function memeDetails(codingMemesAPI) {
-  currentMeme = codingMemesAPI;
-  const name = document.querySelector(".name");
+
+function memeDetails(data) {
+  currentMeme = memeData;
+  const name = document.getElementById("name");
+  name.textContent = data.name;
 
   const image = document.querySelector(".detail-image");
-  image.src = currentMeme.url;
+  image.src = data.image;
+
+  const topText = document.querySelector(".top-comment");
+  topText.textContent = data.top_comment;
+  console.log(topText);
+  const bottomComment = document.querySelector(".bottom-comment");
+  bottomComment.textContent = data.bottom_comment;
+
+  // const rating = document.getElementById("rating-display");
+  // rating.textContent = data.rating;
 }
+
+// function memeDetails(memeData) {
+//   currentMeme = memeData;
+
+//   const image = document.querySelector(".detail-image");
+//   image.src = currentMeme.image;
+//   // console.log(memeData)
+//   let title = document.getElementById("title");
+//   title.textContent = memeData.title;
+// console.log(title);
+//}
+
+//function to add new meme
+const addNewMeme = () => {
+  const newMeme = document.getElementById("new-meme");
+  newMeme.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newName = document.getElementById("new-name");
+    const newImg = document.getElementById("new-image");
+    const newTopText = document.getElementById("new-top-comment");
+    const newBottomText = document.getElementById("new-bottom-comment");
+
+    //new object holder for submitted information
+    const newData = {
+      name: newName,
+      top_comment: newTopText,
+      bottom_comment: newBottomText,
+      image: newImage,
+    };
+    //re-fetch local server to add object data to
+    fetch(memesAPI,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body : JSON.stringify(newData)})
+          .then(resp=>resp.json())
+          .then(responseData=>{
+            console.log("Response:",responseData)
+          })
+          .catch(error=>
+            console.error("Error:",error))
+  });
+};
